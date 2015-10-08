df512cd9d4bf7407c358e3f31a872db9f47d7258
smartnetworks <smartnetworks@users.noreply.github.com>
2015-10-08 08:41:28 +0200

    = project: Bump version to 1.3.3-smartnetworks-p3

--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -10,7 +10,7 @@ import com.typesafe.sbt.osgi.SbtOsgi
 import SbtOsgi._
 
 object BuildSettings {
-  val VERSION = "1.3.3-smartnetworks-p1"
+  val VERSION = "1.3.3-smartnetworks-p3"
 
   lazy val basicSettings = seq(
     version := NightlyBuildSupport.buildVersion(VERSION),
